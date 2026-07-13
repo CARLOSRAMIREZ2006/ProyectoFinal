01d27906e314fe656c9885997054ce7179d4f05c
CARLOSRAMIREZ2006 <carl.ramirezg@duocuc.cl>
2026-07-12 22:06:53 -0400

    Mejorar PacienteDTO

--- src/main/java/com/hospital_vm_vl/hospital_vm/dto/PacienteDTO.java
+++ src/main/java/com/hospital_vm_vl/hospital_vm/dto/PacienteDTO.java
@@ -2,15 +2,18 @@ package com.hospital_vm_vl.hospital_vm.dto;
 
 import lombok.Data;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class PacienteDTO {
     private Long id;
 
-    @NotNull(message = "El nombre es obligatorio")
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
     private String nombre;
 
+    @NotBlank(message = "El email es obligatorio") // Añadido @NotBlank
     @Email(message = "Debe ser un email válido")
     private String email;
 
